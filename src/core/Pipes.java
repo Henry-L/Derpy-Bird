929bb5646c94c6f1eaed7d69a04fc44e4c8494fa
unknown <henry.lee@TXWR341C5TQ.Taxware.local>
2014-02-11 16:00:41 -0500

    Pipe sprites added. Pipe class started.

---
+++ src/core/Pipes.java
@@ -0,0 +1,15 @@
+package core;
+
+import javax.swing.JPanel;
+
+public class Pipes extends JPanel{
+	
+	private int height = (int) (900*0.75);
+	private int width = 90;
+	private int spaceApart = 65;
+	
+	
+	
+		
+	
+}
